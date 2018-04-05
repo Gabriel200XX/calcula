0929d0351e59f2c8d01aa35b0694fbe347111b26
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2018-04-05 11:41:37 -0300

    Implementada a função de adicionar

---
+++ src/js/calculadora.js
@@ -0,0 +1,21 @@
+var Calculadora = {
+  adicionar: function(n1, n2) {
+    if(isNaN(n1) || isNaN(n2)) {
+      return 0;
+    }
+    return parseFloat(n1) + parseFloat(n2);
+  },
+  subtrair: function(n1, n2) {
+    return 0;
+  },
+  dividir: function(n1, n2) {
+    return 0;
+  },
+  multiplicar: function(n1, n2) {
+    return 0;
+  },
+}
+
+if(typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = Calculadora;
+}
