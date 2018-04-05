1eb0ca2e7350c383c1e6e920b44521a476477001
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2018-04-05 15:00:08 -0300

    Refatorado o objeto Calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,30 +1,56 @@
 var Calculadora = {
+  ADICAO: '+',
+  SUBTRACAO: '-',
+  DIVISAO: '/',
+  MULTIPLICACAO: '*',
+
   adicionar: function(n1, n2) {
-    if(isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-    return parseFloat(n1) + parseFloat(n2);
+    return Calculadora.calcular(n1, n2, Calculadora.ADICAO);
   },
   subtrair: function(n1, n2) {
-    if(isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-    return n1 - n2;
+    return Calculadora.calcular(n1, n2, Calculadora.SUBTRACAO);
   },
   dividir: function(n1, n2) {
-    if(isNaN(n1) || isNaN(n2)) {
-      return 0;
-    } else if(n2 === 0) {
+    if(n2 === 0) {
       return "Erro";
     }
-    return n1 / n2;
+    return Calculadora.calcular(n1, n2, Calculadora.DIVISAO);
   },
   multiplicar: function(n1, n2) {
+    return Calculadora.calcular(n1, n2, Calculadora.MULTIPLICACAO);
+  },
+
+  calcular: function(n1, n2, operacao){
+    var resultado = 0;
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+
     if(isNaN(n1) || isNaN(n2)) {
       return 0;
     }
-    return n1 * n2;
-  },
+
+    switch (operacao) {
+      case Calculadora.ADICAO:
+        resultado = n1 + n2;
+        break;
+
+      case Calculadora.SUBTRACAO:
+        resultado = n1 - n2;
+        break;
+
+      case Calculadora.DIVISAO:
+        resultado = n1 / n2;
+        break;
+
+      case Calculadora.MULTIPLICACAO:
+        resultado = n1 * n2;
+        break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
 
 if(typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
